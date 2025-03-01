9059fdd519b0032e47155dac7776cba24fd98af8
Paprika Seidler <pas002@edu.zealand.dk>
2025-03-01 16:20:18 +0100

    Add project files.

---
+++ TrophyLib/Trophy.cs
@@ -0,0 +1,43 @@
+﻿namespace TrophyLib
+{
+    public class Trophy
+    {
+        private string? _competition;
+        private int _year;
+
+        public int Id { get; set; }
+        public string Competition
+        {
+            get => _competition;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Competition name cannot be null.");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Competition name must be at least 3 characters long.");
+                }
+                _competition = value;
+            }
+        }
+        public int Year
+        {
+            get => _year;
+            set
+            {
+                if (value < 1970 || value > 2025)
+                {
+                    throw new ArgumentOutOfRangeException("Year must be after 1970 and before 2025");
+                }
+                _year = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Competition} {Year}";
+        }
+    }
+}
---
+++ TrophyLibTests/TrophyTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrophyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrophyLib.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void TrophyCompetitionTest()
+        {
+            Trophy trophy = new Trophy();
+            trophy.Competition = "World Cup";
+            Assert.AreEqual("World Cup", trophy.Competition);
+            Assert.ThrowsException<ArgumentNullException>(() => trophy.Competition = null);
+            Assert.ThrowsException<ArgumentException>(() => trophy.Competition = "ab");
+        }
+
+        [TestMethod()]
+        public void TrophyYearTest()
+        {
+            Trophy trophy = new Trophy();
+            trophy.Year = 2025;
+            Assert.AreEqual(2025, trophy.Year);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 1969);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 2026);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Trophy trophy = new Trophy();
+            trophy.Competition = "World Cup";
+            trophy.Year = 2022;
+            Assert.AreEqual("World Cup 2022", trophy.ToString());
+        }
+    }
+}
